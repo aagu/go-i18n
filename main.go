25ab50619e70d8c703ff237b35429813b9784e8b
aagu <aaguisme@gmail.com>
2021-03-01 14:42:08 +0800

    feat: export cmd tool
    export go-i18n cmd tool

--- main.go
+++ main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"github.com/aagu/go-i18n/pkg/translation"
-	lang "golang.org/x/text/language"
+	"github.com/aagu/go-i18n/cmd/extract"
+	"github.com/spf13/cobra"
+	"os"
 )
 
-var (
-	jan      = translation.Message{ID: "January", Text: "January"}
-	greeting = translation.Message{ID: "Hello", Text: "Hello"}
-	format   = translation.Message{ID: "DayOfMonth", Text: "The {{.Day}}(th) day of {{.Month}}"}
-	ways     = translation.Message{ID: "TwoWay", Text: "One way is to {{.One}}, the other is to {{.Other}}"}
-	roma     = translation.Message{ID: "roma", Text: "roma"}
-	paris    = translation.Message{ID: "paris", Text: "paris"}
-	text     = translation.Message{ID: "text", Text: "{{.}}"}
-)
+var command = cobra.Command{
+	Use:   "go-i18n",
+	Short: "Command line tool for go-i18n",
+}
+
+func init() {
+	command.AddCommand(extract.NewExtractCommand())
+}
 
 func main() {
-	translation.SetDefaultLocale(lang.English)
-	translation.LoadTranslations(`C:\Users\aagui\IdeaProjects\go-i18n\i18n`)
-	fmt.Println(greeting.Translate(lang.SimplifiedChinese))
-	fmt.Println(format.FormatTranslate(lang.SimplifiedChinese, translation.TemplateData{"Day": 2, "Month": jan}))
-	fmt.Println(ways.Format(translation.TemplateData{"One": roma, "Other": paris}))
-	fmt.Println(text.FormatTranslate(lang.SimplifiedChinese, &greeting))
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
